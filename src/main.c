b12d255b416450d5264149f7cfd6b9a751c46d2e
spevnev <spevnev16@gmail.com>
2024-09-23 10:58:49 -0400

    Parse input

--- src/main.c
+++ src/main.c
@@ -17,8 +17,84 @@ static const char *TITLE = "eBPF Graph";
 
 static const Color BACKGROUND = {0x18, 0x18, 0x18, 0xff};
 
+typedef struct {
+    uint64_t timestamp;
+    int32_t cgroup;
+    uint32_t latency;
+} Entry;
+
 static void usage(const char *program) {
     printf("usage: %s <filename>\n", program);
+}
+
+static const char *skip_field(const char *ch) {
+    while (*ch != ' ' && *ch != '\0') ch++;
+    assert(*ch != '\0');
+    return ch;
+}
+
+static const char *llong_field(long long *ret, const char *ch) {
+    while (*ch == ' ') ch++;
+
+    char *end = NULL;
+    *ret = strtoll(ch, &end, 10);
+    assert(end != NULL && ch < end);
+
+    return end;
+}
+
+static int parse_output(Entry **ret_entries, size_t *ret_entries_len, FILE *fp) {
+    assert(ret_entries != NULL && ret_entries_len != NULL && fp != NULL);  // TODO: error vs assert
+
+    const int LINES = 186135;
+
+    size_t entries_len = LINES;
+    Entry *entries = malloc(entries_len * sizeof(*entries));  // TODO: just mmap?
+    Entry *entry = entries;
+
+    bool skipped_header = false;
+    ssize_t read;
+    char *line;
+    size_t line_len;
+    while ((read = getline(&line, &line_len, fp)) != -1) {
+        if (!skipped_header) {
+            skipped_header = true;
+            continue;
+        }
+
+        const char *ch = line;
+
+        // TIME
+        ch = skip_field(ch);
+
+        // PREV_CGROUP
+        ch = skip_field(ch);
+
+        // CGROUP
+        long long cgroup;
+        ch = llong_field(&cgroup, ch);
+        assert(0 <= cgroup && cgroup <= INT32_MAX);
+        entry->cgroup = cgroup;
+
+        // RUNQ_LATENCY
+        long long latency;
+        ch = llong_field(&latency, ch);
+        assert(0 <= latency && latency <= UINT32_MAX);
+        entry->latency = latency;
+
+        // TS
+        long long timestamp;
+        ch = llong_field(&timestamp, ch);
+        assert(0 <= timestamp);
+        entry->timestamp = timestamp;
+
+        entry++;
+    }
+    if (line) free(line);
+
+    *ret_entries = entries;
+    *ret_entries_len = entries_len;
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
@@ -42,6 +118,13 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
+    Entry *entries;
+    size_t entries_len;
+    if (parse_output(&entries, &entries_len, fp) != 0) {
+        fprintf(stderr, "ERROR: unable to parse file.\n");
+        return EXIT_FAILURE;
+    }
+
     fclose(fp);
 
     SetTraceLogLevel(LOG_WARNING);
@@ -57,5 +140,8 @@ int main(int argc, char *argv[]) {
     }
 
     CloseWindow();
+
+    free(entries);
+
     return EXIT_SUCCESS;
 }
