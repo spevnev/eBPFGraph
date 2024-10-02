74fc11b90b2e9ce75d57403d4d7ad2f5c3e5b930
spevnev <spevnev16@gmail.com>
2024-10-01 21:00:13 -0400

    Update eBPF

--- ebpf/latency.bpf.c
+++ ebpf/latency.bpf.c
@@ -7,38 +7,27 @@
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
-#define RATE_LIMIT_NS 10
-#define MAX_ENTRIES 8192
-#define RINGBUF_SIZE_BYTES 65535
+#define MAX_RUNQ_ENTRIES 8192
+#define MAX_EVENT_ENTRIES 8192
 
 struct {
     __uint(type, BPF_MAP_TYPE_HASH);
-    __uint(max_entries, MAX_ENTRIES);
+    __uint(max_entries, MAX_RUNQ_ENTRIES);
     __type(key, __u32);
     __type(value, __u64);
 } runq_enqueued SEC(".maps");
 
 struct {
     __uint(type, BPF_MAP_TYPE_RINGBUF);
-    __uint(max_entries, 256 * 1024);
+    __uint(max_entries, MAX_EVENT_ENTRIES);
 } events SEC(".maps");
-
-struct {
-    __uint(type, BPF_MAP_TYPE_PERCPU_HASH);
-    __uint(max_entries, MAX_ENTRIES);
-    __type(key, __u64);
-    __type(value, __u64);
-} cgroup_id_to_last_event_ts SEC(".maps");
 
 void bpf_rcu_read_lock(void) __ksym;
 void bpf_rcu_read_unlock(void) __ksym;
 
 __u64 get_task_cgroup_id(struct task_struct *task) {
-    struct css_set *cgroups;
-    __u64 cgroup_id;
     bpf_rcu_read_lock();
-    cgroups = task->cgroups;
-    cgroup_id = cgroups->dfl_cgrp->kn->id;
+    __u64 cgroup_id = task->cgroups->dfl_cgrp->kn->id;
     bpf_rcu_read_unlock();
     return cgroup_id;
 }
@@ -63,9 +52,7 @@ int tp_sched_switch(__u64 *ctx) {
 
     // fetch timestamp of when the next task was enqueued
     __u64 *tsp = bpf_map_lookup_elem(&runq_enqueued, &next_pid);
-    if (tsp == NULL) {
-        return 0;  // missed enqueue
-    }
+    if (tsp == NULL) return 0;  // missed enqueue
 
     // calculate runq latency before deleting the stored timestamp
     __u64 now = bpf_ktime_get_ns();
@@ -77,28 +64,13 @@ int tp_sched_switch(__u64 *ctx) {
     __u64 prev_cgroup_id = get_task_cgroup_id(prev);
     __u64 cgroup_id = get_task_cgroup_id(next);
 
-    // per-cgroup-id-per-CPU rate-limiting
-    // to balance observability with performance overhead
-    __u64 *last_ts = bpf_map_lookup_elem(&cgroup_id_to_last_event_ts, &cgroup_id);
-    __u64 last_ts_val = last_ts == NULL ? 0 : *last_ts;
-
-    // check the rate limit for the cgroup_id in consideration
-    // before doing more work
-    if (now - last_ts_val < RATE_LIMIT_NS) {
-        // Rate limit exceeded, drop the event
-        return 0;
-    }
-
     struct runq_event *event = bpf_ringbuf_reserve(&events, sizeof(*event), 0);
-
     if (event) {
         event->prev_cgroup_id = prev_cgroup_id;
         event->cgroup_id = cgroup_id;
         event->runq_latency = runq_latency;
         event->ts = now;
         bpf_ringbuf_submit(event, 0);
-        // Update the last event timestamp for the current cgroup_id
-        bpf_map_update_elem(&cgroup_id_to_last_event_ts, &cgroup_id, &now, BPF_ANY);
     }
 
     return 0;
